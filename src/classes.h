e2f4a2a623303d96df0289bc9f7fe7101bae6569
C-Gauth <114773300+C-Gauth@users.noreply.github.com>
2023-04-19 00:07:13 -0700

    Add files via upload

---
+++ src/classes.h
@@ -0,0 +1,162 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Bullet : public sf::Drawable, public sf::Transformable
+{
+public:
+	Bullet()
+	{
+		if (!texture.loadFromFile("bullet.png"))
+		{
+			std::cerr << "Error loading texture\n";
+			exit(1);
+		}
+		sprite.setTexture(texture);
+
+		// Set hitbox size and position
+		hitbox.setSize(sf::Vector2f(texture.getSize()));
+		hitbox.setOrigin(hitbox.getSize() / 2.f);
+		hitbox.setPosition(getPosition());
+	}
+
+	void move()
+	{
+		// Move bullet up
+		sf::Vector2f position = getPosition();
+		setPosition(sf::Vector2f(position.x, position.y - speed));
+	}
+
+	sf::FloatRect getHitbox() const
+	{
+		return hitbox.getGlobalBounds();
+	}
+
+	void draw(sf::RenderTarget& target, sf::RenderStates states) const override
+	{
+		target.draw(sprite, states);
+	}
+
+private:
+	float speed = 10.f;
+	sf::RectangleShape hitbox;
+	sf::Sprite sprite;
+	sf::Texture texture;
+};
+
+class Ship : public sf::Drawable, public sf::Transformable
+{
+
+public:
+	vector<Bullet> bullets; // vector to store bullets
+
+	Ship()
+	{
+		if (!texture.loadFromFile("ship.png"))
+		{
+			std::cerr << "Error loading texture\n";
+			exit(1);
+		}
+		sprite.setTexture(texture);
+
+		// Set hitbox size and position
+		hitbox.setSize(sf::Vector2f(texture.getSize()));
+		hitbox.setOrigin(hitbox.getSize() / 2.f);
+		hitbox.setPosition(getPosition());
+	}
+
+	Ship(string pngName) // ship constructor given PNG image
+	{
+		if (!texture.loadFromFile(pngName))
+		{
+			std::cerr << "Error loading texture\n";
+			exit(1);
+		}
+		sprite.setTexture(texture);
+
+		// Set hitbox size and position
+		hitbox.setSize(sf::Vector2f(texture.getSize()));
+		hitbox.setOrigin(hitbox.getSize() / 2.f);
+		hitbox.setPosition(getPosition());
+	}
+
+	sf::FloatRect getHitbox() const // function to get the hitbox of the ship
+	{
+		return hitbox.getGlobalBounds();
+	}
+
+	void setPosition(sf::Vector2f position) // to move the ship
+	{
+		sf::Transformable::setPosition(position); // update position
+		hitbox.setPosition(position);			  // update hitbox position
+		sprite.setPosition(position);			  // update sprite position
+	}
+
+	void draw(sf::RenderTarget& target, sf::RenderStates states) const override
+	{
+		target.draw(sprite, states);
+	}
+
+	sf::Sprite getSprite()
+	{
+		return sprite;
+	};
+
+	void createBullet() // function to create a new bullet
+	{
+		Bullet bullet;
+		bullet.setPosition(getPosition());
+		bullets.push_back(bullet);
+	}
+
+	void updateBullets()
+	{
+		for (size_t i = 0; i < bullets.size(); i++)
+		{
+			bullets[i].move();
+			if (bullets[i].getPosition().y < 0) // remove bullet if it goes off screen
+			{
+				bullets.erase(bullets.begin() + i);
+			}
+		}
+	}
+
+protected:
+	sf::RectangleShape hitbox; // hitbox of the ship
+	sf::Sprite sprite;		   // sprite of the ship
+	sf::Texture texture;	   //ship texture img
+};
+
+/*
+	// create the pause screen rectangle
+	sf::RectangleShape pauseScreen(sf::Vector2f(window.getSize().x / 2.f, window.getSize().y / 2.f));
+	pauseScreen.setFillColor(sf::Color(0, 0, 0, 128)); // semi-transparent black background for the pause screen
+	pauseScreen.setOrigin(pauseScreen.getSize() / 2.f);
+	pauseScreen.setPosition(windowSize.x / 2.f, windowSize.y / 2.f);
+
+
+	sf::Font font;
+	font.loadFromFile("arial.ttf");
+
+	sf::Text resumeText("Resume", font, 250);
+	resumeText.setFillColor(sf::Color::White);
+	resumeText.setPosition(windowSize.x / 2.f - resumeText.getLocalBounds().width / 2.f, windowSize.y / 2.f - 50);
+
+	sf::Text exitText("Exit Game", font, 250);
+	exitText.setFillColor(sf::Color::White);
+	exitText.setPosition(windowSize.x / 2.f - exitText.getLocalBounds().width / 2.f, windowSize.y / 2.f + 50);
+*/
+
+/*
+		else // draw the pause screen and buttons if the game is paused
+		{
+			window.setMouseCursorVisible(true); // show the cursor when paused
+
+			window.draw(pauseScreen);
+			window.draw(resumeText);
+			window.draw(exitText);
+			window.display();
+
+		}
+*/
--- src/main.cpp
+++ src/main.cpp
@@ -1,100 +1,85 @@
+#include "classes.h"
 #include <SFML/Graphics.hpp>
+#include <SFML/Window.hpp>
 #include <cmath>
 
 int main()
 {
 	sf::RenderWindow window(sf::VideoMode::getFullscreenModes()[0], "Space Invaders", sf::Style::Fullscreen);
 	window.setMouseCursorVisible(false);
-
-	sf::Texture shipTexture;
-	shipTexture.loadFromFile("ship.png");
-
-	sf::Sprite shipSprite(shipTexture);
-	sf::Vector2u windowSize = window.getSize();
-	float xPos = windowSize.x / 2.f;
-	float yPos = 0.9f * windowSize.y;
-	shipSprite.setPosition(xPos, yPos);
-
-	float shipSpeed = 5.f;
+	window.setFramerateLimit(60);
 
 	bool paused = false;
 
-	// create the pause screen rectangle
-	sf::RectangleShape pauseScreen(sf::Vector2f(window.getSize().x / 2.f, window.getSize().y / 2.f));
-	pauseScreen.setFillColor(sf::Color(0, 0, 0, 128)); // semi-transparent black background for the pause screen
-	pauseScreen.setOrigin(pauseScreen.getSize() / 2.f);
-	pauseScreen.setPosition(windowSize.x / 2.f, windowSize.y / 2.f);
-
-	sf::Font font;
-	font.loadFromFile("arial.ttf");
-
-	sf::Text resumeText("Resume", font, 250);
-	resumeText.setFillColor(sf::Color::White);
-	resumeText.setPosition(windowSize.x / 2.f - resumeText.getLocalBounds().width / 2.f, windowSize.y / 2.f - 50);
-
-	sf::Text exitText("Exit Game", font, 250);
-	exitText.setFillColor(sf::Color::White);
-	exitText.setPosition(windowSize.x / 2.f - exitText.getLocalBounds().width / 2.f, windowSize.y / 2.f + 50);
+	Ship playerShip;
 
 	while (window.isOpen())
 	{
 		sf::Event event;
 		while (window.pollEvent(event))
 		{
-			if (event.type == sf::Event::Closed)
+			switch (event.type)
 			{
-				window.close();
-			}
-
-			if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Escape)
-			{
-				paused = !paused;
-			}
-
-			if (event.type == sf::Event::MouseButtonPressed && event.mouseButton.button == sf::Mouse::Left && paused)
-			{
-				sf::Vector2i mousePosition = sf::Mouse::getPosition(window);
-
-				if (resumeText.getGlobalBounds().contains(mousePosition.x, mousePosition.y))
-				{
-					paused = false;
-				}
-				else if (exitText.getGlobalBounds().contains(mousePosition.x, mousePosition.y))
-				{
+				case sf::Event::Closed:
 					window.close();
-				}
+					break;
+
+				case sf::Event::KeyPressed:
+					switch (event.key.code)
+					{
+						case sf::Keyboard::Escape:
+							paused = !paused;
+							break;
+
+						default:
+							break;
+					}
+					break;
+
+				case sf::Event::MouseButtonPressed:
+					switch (event.mouseButton.button)
+					{
+						case sf::Mouse::Left:
+							if (paused)
+							{
+								// code for resume and exit buttons
+							}
+							if (!paused)
+							{
+								// Shoot bullet
+								Bullet bullet;
+								bullet.setPosition(playerShip.getPosition());
+								playerShip.bullets.push_back(bullet);
+							}
+							break;
+
+						default:
+							break;
+					}
+				default:
+					break;
 			}
 		}
 
 		if (!paused)
 		{
+			window.setMouseCursorVisible(false);
 			sf::Vector2i mousePosition = sf::Mouse::getPosition(window);
-			window.setMouseCursorVisible(false); // dont show the cursor when not paused
-
-			float dx = mousePosition.x - shipSprite.getPosition().x;
-			float dy = mousePosition.y - shipSprite.getPosition().y;
-
-			float distance = std::sqrt(dx * dx + dy * dy);
-
-			if (distance != 0)
-			{
-				dx /= distance;
-				dy /= distance;
-			}
-
-			shipSprite.move(shipSpeed * dx, shipSpeed * dy);
+			playerShip.setPosition(sf::Vector2f(mousePosition.x - playerShip.getSprite().getGlobalBounds().width / 2.f, mousePosition.y - playerShip.getSprite().getGlobalBounds().height / 2.f));
 
 			window.clear();
-			window.draw(shipSprite);
+			//window.draw(playerShip);
 			window.display();
+			for (auto& bullet : playerShip.bullets)
+			{
+				window.draw(bullet);
+			}
+			playerShip.updateBullets();
 		}
-		else // draw the pause screen and buttons if the game is paused
+
+		else
 		{
-			window.setMouseCursorVisible(true); // show the cursor when paused
-			window.draw(pauseScreen);
-			window.draw(resumeText);
-			window.draw(exitText);
-			window.display();
+			window.setMouseCursorVisible(true);
 		}
 	}
 
